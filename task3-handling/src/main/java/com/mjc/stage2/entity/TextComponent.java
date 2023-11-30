593fee014db38c8607eaa95f8e10f5036c5b8440
THETOUCH <super.ilgiznex@gmail.com>
2023-12-01 00:34:58 +0600

    12345

--- task3-handling/src/main/java/com/mjc/stage2/entity/TextComponent.java
+++ task3-handling/src/main/java/com/mjc/stage2/entity/TextComponent.java
@@ -3,8 +3,37 @@ package com.mjc.stage2.entity;
 import java.util.ArrayList;
 import java.util.List;
 
-public class TextComponent {
+public class TextComponent extends AbstractTextComponent {
     protected List<AbstractTextComponent> componentList = new ArrayList<>();
     private int size = 0;
+
+    public TextComponent(TextComponentType componentType) {
+        super(componentType);
+    }
+
+    @Override
+    public String operation() {
+        StringBuilder result = new StringBuilder();
+        //size = componentList.size();
+        for (AbstractTextComponent abstractTextComponent : componentList) {
+            result.append(abstractTextComponent.operation());
+        }
+        return result.toString();
+    }
+
+    @Override
+    public void add(AbstractTextComponent textComponent) {
+        componentList.add(textComponent);
+    }
+
+    @Override
+    public void remove(AbstractTextComponent textComponent) {
+        componentList.remove(textComponent);
+    }
+
+    @Override
+    public int getSize() {
+        return componentList.size();
+    }
 // Write your code here!
 }
--- task3-handling/src/main/java/com/mjc/stage2/parser/WordParser.java
+++ task3-handling/src/main/java/com/mjc/stage2/parser/WordParser.java
@@ -1,7 +1,18 @@
 package com.mjc.stage2.parser;
 
 
-public class WordParser {
+import com.mjc.stage2.entity.AbstractTextComponent;
+import com.mjc.stage2.entity.SymbolLeaf;
+import com.mjc.stage2.entity.TextComponentType;
+
+public class WordParser extends AbstractTextParser {
+    @Override
+    public void parse(AbstractTextComponent abstractTextComponent, String string) {
+        char[] array = string.toCharArray();
+        for (char ch : array) {
+            abstractTextComponent.add(new SymbolLeaf(ch, TextComponentType.WORD));
+        }
+    }
 
     // Write your code here!
 
